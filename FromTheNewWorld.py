a79137dd59dd5856cea3c29e9a6325131f8ec15e
BananeChocolat <jy.aurelien@gmail.com>
2024-03-28 15:29:17 +0100

    beginning of collapsing mechanism

--- FromTheNewWorld.py
+++ FromTheNewWorld.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as r
 from pprint import pprint
 
 green = "\u001b[32m"
@@ -10,13 +11,22 @@ out_size = 15
 
 # === Script ===
 
+def saver(chunk, filename="test_file") -> None :
+    """ Sauvegarde un chunk dans un fichier .vox"""
+    save = Entity().from_dense(chunk)
+    save.set_palette_from_file('palette.png')
+    save.save(f'./vox/{filename}.vox')
+    print(f"[{green}INFO{white}] Chunk saved as {filename}.vox")
+
+
 class Cell:
     """Classe qui définit un cube de n*n de voxels"""
     def __init__(self, obj, model_length:int, n:int=default_n):
         self.obj = np.zeros(shape=(n,n,n), dtype=int)
         self.dim = n
         self.possible = np.full(fill_value="", dtype=str, shape=(model_length))
-    
+        self.collapsed = False
+
     def __str__(self):
         return self.possible
 
@@ -64,7 +74,30 @@ def generate_struct(model:dict, size:int=16, n:int=default_n):
     default_cell = Cell(np.zeros(shape=(n,n,n), dtype=int), len(model), n)
     default_cell.possible = np.array(list(model.keys()))
     cell_tensor = np.full(fill_value=default_cell, dtype=Cell, shape=(size, size, size))
-    pprint(cell_tensor)
+
+    # TODO
+    def collapse():
+        """ WORK IN PROGRESS - Réduit la cellule de plus faible entropie à un état"""
+        tensor_size = cell_tensor.shape
+        entropy_map = np.zeros(shape=tensor_size, dtype=int)
+        for x in range(tensor_size[0]):
+            for y in range(tensor_size[1]):
+                for z in range(tensor_size[2]):
+                    entropy_map[x][y][z] = len(cell_tensor[x][y][z].possible)
+        entropy_map[15][14][13] = 0
+        min_entropy_index = np.unravel_index(np.argmin(entropy_map), tensor_size)
+        print(f'{min_entropy_index = }')
+
+
+        # test
+        # print(f'{r.choice(list(filter(lambda x: x!="",cell_tensor[min_entropy_index].possible))) = }')
+
+
+    collapse()
+
+    # pprint(cell_tensor)
+
+
 
 
 
@@ -73,3 +106,12 @@ def generate_struct(model:dict, size:int=16, n:int=default_n):
 
 
 generate_struct(create_model(np.random.randint(0, 2, size=(10,10,10))))
+
+
+test = np.zeros((2,2,2), int)
+test[0][0] = 1
+
+test1 = list(filter(lambda x: x!=1, test))
+
+
+print(test)
